4d5b2d6a3f82b7edb62287cc762e388429d01387
Sudharsan0897 <sandon506@gmail.com>
2026-03-04 15:57:37 +0530

    new commit

--- database.py
+++ database.py
@@ -1,10 +1,11 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-
 from sqlalchemy.orm import sessionmaker
 
 # UPDATE YOUR POSTGRES CREDENTIALS
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:San1320!@localhost:5432/tts_project"
+SQLALCHEMY_DATABASE_URL = os.getenv('DATABASE_URL')
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
